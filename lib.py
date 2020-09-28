c277a6ab9b5f20a49e5323d43696ba8aa3cf14b3
Stephen DeGrace <stephen.bendl@gmail.com>
2020-09-28 09:12:24 -0400

    Initial commit

---
+++ lib.py
@@ -0,0 +1,284 @@
+import yaml
+import pandas as pd
+
+
+class GameObject:
+
+    def __init__(self, registered=False, built=False, _registry_lookup=None):
+        self.registered = registered
+        self.registry_lookup = _registry_lookup
+        self.built = built
+
+    def build(self, game):
+        self.built = True
+
+
+class Character(GameObject):
+
+    def __init__(self, name=None, soul=None, body=None, backstory=None):
+        super().__init__()
+        self.soul = soul
+        self.body = body
+        self.name = name
+        self.backstory=backstory
+
+    def gain_skill(self, skill, points):
+        self.soul.skills[skill] += points * self.soul.get_governing_deity_favor(skill)
+
+    def gain_favor(self, deity, points):
+        self.soul.deity_favor[deity] += points
+
+
+class Soul(GameObject):
+
+
+    def __init__(self, odin_favor=0, thor_favor=0, loki_favor=0,
+                 tactics=0, lore=0, perception=0, investigation=0, deduction=0,
+                 power=0, athletics=0, healing=0, intimidation=0, performance=0,
+                 accuracy=0, speed=0, persuasion=0, deception=0, stealth=0):
+        super().__init__()
+        self.deity_favor = {'odin': odin_favor,
+                            'thor': thor_favor,
+                            'loki': loki_favor}
+        self.skills = {'tactics': tactics, 'lore': lore, 'perception': perception, 'investigation': investigation, 'deduction': deduction,
+                       'power': power, 'athletics': athletics, 'healing': healing, 'intimidation': intimidation, 'performance': performance,
+                       'accuracy': accuracy, 'speed': speed, 'persuasion': persuasion, 'deception': deception, 'stealth': stealth}
+
+    def get_governing_deity(self, skill):
+        governing_deities = {'tactics': 'odin', 'lore': 'odin', 'perception': 'odin', 'investigation': 'odin',
+                             'deduction': 'odin',
+                             'power': 'thor', 'athletics': 'thor', 'healing': 'thor', 'intimidation': 'thor',
+                             'performance': 'thor',
+                             'accuracy': 'loki', 'speed': 'loki', 'persuasion': 'loki', 'deception': 'loki',
+                             'stealth': 'loki'}
+        return governing_deities[skill]
+
+    def get_governing_deity_favor(self, skill):
+        return self.deity_favor[self.get_governing_deity(skill)]
+
+    @property
+    def odin_favor(self):
+        return self.deity_favor['odin']
+
+    @property
+    def thor_favor(self):
+        return self.deity_favor['thor']
+
+    @property
+    def loki_favor(self):
+        return self.deity_favor['loki']
+
+    @property
+    def tactics(self):
+        return self.skills['tactics']
+
+    @property
+    def lore(self):
+        return self.skills['lore']
+
+    @property
+    def perception(self):
+        return self.skills['perception']
+
+    @property
+    def investigation(self):
+        return self.skills['investigation']
+
+    @property
+    def deduction(self):
+        return self.skills['deduction']
+
+    @property
+    def power(self):
+        return self.skills['power']
+
+    @property
+    def athletics(self):
+        return self.skills['athletics']
+
+    @property
+    def healing(self):
+        return self.skills['healing']
+
+    @property
+    def intimidation(self):
+        return self.skills['intimidation']
+
+    @property
+    def performance(self):
+        return self.skills['performance']
+
+    @property
+    def accuracy(self):
+        return self.skills['accuracy']
+
+    @property
+    def speed(self):
+        return self.skills['speed']
+
+    @property
+    def persuasion(self):
+        return self.skills['persuasion']
+
+    @property
+    def deception(self):
+        return self.skills['deception']
+
+    @property
+    def stealth(self):
+        return self.skills['stealth']
+
+
+class BodyPlan(GameObject):
+
+    def __init__(self, name=None, body_plan=None):
+        super().__init__(_registry_lookup='BodyPlan_' + name)
+        self.body_plan = body_plan
+        self.name = name
+
+    @staticmethod
+    def from_yaml(filename):
+        with open(filename) as file:
+            loaded_yamls = yaml.safe_load_all(file)
+
+            bodies = [BodyPlan(bp['name'], bp['body_plan']) for bp in loaded_yamls if bp is not None]
+        return bodies
+
+    def build(self, game):
+        self.body_plan = self._build(self.body_plan, game)
+        super(BodyPlan, self).build(game)
+
+    @staticmethod
+    def _build(body_plan, game):
+        if type(body_plan) == dict:
+            return {game.get_object('BodyPart_'+key): BodyPlan._build(val, game) for key, val in body_plan.items()}
+        elif type(body_plan) == str:
+            return game.get_object('BodyPart_'+body_plan)
+        elif type(body_plan) == list:
+            return [BodyPlan._build(item, game) for item in body_plan]
+        else:
+            raise Exception('Uninterpretable BodyPlan: ' + str(body_plan))
+
+
+class Body(GameObject):
+
+    def __init__(self, name=None, body_plan=None):
+        super().__init__(_registry_lookup='Body_' + name)
+        self.body_plan = body_plan
+        self.name = name
+
+    @staticmethod
+    def from_yaml(filename):
+        with open(filename) as file:
+            loaded_yamls = yaml.safe_load_all(file)
+
+            bodies = [Body(bp['name'], bp['body_plan']) for bp in loaded_yamls if bp is not None]
+        return bodies
+
+    def build(self, game):
+        self.body_plan = game.get_object('BodyPlan_'+self.body_plan)
+        super(Body, self).build(game)
+
+class BodyPart(GameObject):
+
+    def __init__(self, name=None, layers=None, can_grasp=None, grasped_object=None, functioning=None):
+        super().__init__(_registry_lookup='BodyPart_' + name)
+        self.name = name
+        self.layers = layers
+        self.can_grasp = can_grasp
+        self.grasped_object = grasped_object
+        self.functioning = functioning
+
+    @staticmethod
+    def from_yaml(filename):
+        with open(filename) as file:
+            loaded_yamls = yaml.safe_load_all(file)
+
+            body_parts = [BodyPart(bp['name'], bp['layers'], bp['can_grasp']) for bp in loaded_yamls if bp is not None]
+        return body_parts
+
+    def build(self, game):
+        super(BodyPart, self).build(game)
+        for layer in self.layers:
+            mat_name = layer['material']
+            mat_ref = game.get_object('Material_'+mat_name)
+            layer['material'] = mat_ref
+
+
+class Organ(GameObject):
+
+    def __init__(self, name=None, material=None, blood_percentage=None, injury_timings=None, current_injury_level=None):
+        super().__init__(_registry_lookup='Organ_' + name)
+        self.name = name
+        self.material = material
+        self.blood_percentage = blood_percentage
+        self.injury_timings = injury_timings
+        self.current_injury_level = current_injury_level
+
+    @staticmethod
+    def from_yaml(filename):
+        with open(filename) as file:
+            loaded_yamls = yaml.safe_load_all(file)
+
+            body_parts = [Organ(organ['name'], organ['material'], organ['blood_percentage'], organ['injury_timings'])
+                          for organ in loaded_yamls if organ is not None]
+        return body_parts
+
+    def build(self, game):
+        super(Organ, self).build(game)
+        self.material = game.get_object('Material_' + self.material)
+
+
+class Material(GameObject):
+
+    def __init__(self, name, density, compressive_yield, compressive_ult, tensile_yield, tensile_ult, shear_yield,
+                 shear_ult, mod_of_elasticity, shear_modulus, strain_at_fracture):
+        super().__init__(_registry_lookup='Material_' + name)
+        self.name = name
+        self.density = density
+        self.compressive_yield = compressive_yield
+        self.compressive_ult = compressive_ult
+        self.tensile_yield = tensile_yield
+        self.tensile_ult = tensile_ult
+        self.shear_yield = shear_yield
+        self.shear_ult = shear_ult
+        self.mod_of_elasticity = mod_of_elasticity
+        self.shear_modulus = shear_modulus
+        self.strain_at_fracture = strain_at_fracture
+
+    @staticmethod
+    def from_yaml(filename):
+        with open(filename) as file:
+            loaded_yamls = yaml.safe_load_all(file)
+            materials = []
+            for mat in loaded_yamls:
+                if mat is not None:
+                    materials.append(Material(
+                        mat['name'], mat['density'], mat['compressive_yield'], mat['compressive_ult'], mat['tensile_yield'],
+                        mat['tensile_ult'],
+                        mat['tensile_yield'] * .577 if 'shear_yield' not in mat.keys() else mat['shear_yield'],
+                        mat['tensile_ult'] * .577 if 'shear_ult' not in mat.keys() else mat['shear_ult'],
+                        mat['mod_of_elasticity'], mat['shear_modulus'], mat['strain_at_fracture']
+                    ))
+        return materials
+
+
+class Game:
+
+    def __init__(self, object_registry: pd.DataFrame=None):
+        if object_registry is None:
+            self.object_registry = pd.DataFrame(columns=['GameObject'])
+        else:
+            self.object_registry = object_registry
+        self.registry_index_counter = 0
+
+    def register_object(self, game_object: GameObject):
+        if game_object.registry_lookup is None:
+            game_object.registry_lookup = str(type(game_object)) + str(self.registry_index_counter)
+        if game_object.registry_lookup not in self.object_registry.index:
+            self.object_registry.loc[game_object.registry_lookup] = game_object
+            game_object.registered = True
+        return game_object
+
+    def get_object(self, lookup):
+        return self.object_registry.loc[lookup].iloc[0]
---
+++ main.py
@@ -0,0 +1,82 @@
+from lib import *
+from random import choice, randint
+import pandas as pd
+
+
+def initialize_game():
+    game = Game()
+    materials = Material.from_yaml('./data/materials.yaml')
+    for material in materials:
+        game_object = game.register_object(material)
+        game_object.build(game)
+
+    organs = Organ.from_yaml('./data/organs.yaml')
+    for organ in organs:
+        game_object = game.register_object(organ)
+        game_object.build(game)
+
+    body_parts = BodyPart.from_yaml('./data/body_parts.yaml')
+    for bp in body_parts:
+        game_object = game.register_object(bp)
+        game_object.build(game)
+
+    body_plans = BodyPlan.from_yaml('./data/body_plans.yaml')
+    for bp in body_plans:
+        game_object = game.register_object(bp)
+        game_object.build(game)
+
+    return game
+
+
+def initialize_player_characters(num, game):
+    with open('./data/backstory.yaml') as file:
+        backstory = yaml.safe_load(file)
+    for c in range(num):
+        character = Character(name='Test', soul=Soul(), body=Body('Human', game.get_object('BodyPlan_tailless_humanoid')))
+        for question in backstory:
+            print(question['text'])
+            for i, (id, option) in enumerate(question['options'].items()):
+                print('\t' + id + ": ", option['text'])
+            response = input('Your choice: ')
+            while response not in question['options']:
+                response = input('Your response must be one the options above... \nYour choice: ')
+            response = question['options'][response]
+            if 'alignment_changes' in response:
+                for deity, points in response['alignment_changes'].items():
+                    character.gain_favor(deity.lower(), points)
+            if 'skill_changes' in response:
+                for skill, points in response['skill_changes'].items():
+                    character.gain_skill(skill.lower(), points)
+            for skill in character.soul.skills:
+                character.gain_skill(skill, randint(0, 4))
+
+
+def test_backstory_balance():
+    character = Character(name='Test', soul=Soul(), body=Body('Human', game.get_object('BodyPlan_tailless_humanoid')))
+    skills = pd.DataFrame(character.soul.skills, index=[0])
+    alignments = pd.DataFrame(character.soul.deity_favor, index=[0])
+    with open('./data/backstory.yaml') as file:
+        backstory = yaml.safe_load(file)
+    for i in range(10000):
+
+        character = Character(name='Test', soul=Soul(), body=Body('Human', game.get_object('BodyPlan_tailless_humanoid')))
+        for question in backstory:
+            response = choice(list(question['options'].values()))
+            if 'alignment_changes' in response:
+                for deity, points in response['alignment_changes'].items():
+                    character.gain_favor(deity.lower(), points)
+            if 'skill_changes' in response:
+                for skill, points in response['skill_changes'].items():
+                    character.gain_skill(skill.lower(), points * 2.2)
+            for skill in character.soul.skills:
+                character.gain_skill(skill, randint(0, 4))
+        skills = pd.concat([skills, pd.DataFrame(character.soul.skills, index=[0])])
+        alignments = pd.concat([alignments, pd.DataFrame(character.soul.deity_favor, index=[0])])
+    skills.to_excel('skills.xlsx')
+    alignments.to_excel('alignments.xlsx')
+
+if __name__ == '__main__':
+    game = initialize_game()
+    initialize_player_characters(1, game)
+
+
